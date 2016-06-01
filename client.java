d8c6b068a24caf9e10c28a4756f7bb434347a815
Deepanshu Sapra <deepanshu3131@gmail.com>
2016-06-01 14:54:41 +0530

    Client File with fully randomized Bots.
    The Clients have 100 threads which act as bots doing randomized transaction.

--- client.java
+++ client.java
@@ -2,7 +2,7 @@
 File: client.java
 Authors: Deepanshu Gupta and Deepanshu Sapra
 Description: Client file which sends requests to the library server
-Last Modified: 31 May 2016
+Last Modified: 1 June 2016
 **************************************************************************************/
 
 import java.io.BufferedReader;
@@ -41,16 +41,54 @@ class ThreadClass extends Thread
 	            OutputStreamWriter osw = new OutputStreamWriter(os);
 	            BufferedWriter bw = new BufferedWriter(osw);
 	 			String[] command = new String[]{"issue ","return ","add ","show "};
-	 			String[] bookname = new String[]{"networking ","algorithms ","mechanics ","thermodynamics ","psdhfjk ","electronics "}; 
+	 			String[] bookname = new String[]{"touwm","awfub","yroei","mydxo","rioap","ydhna","ajdky","bxqks","kdose","ayjgd","lfhwm","bgloj","pbocf","loitp","zykfp","iweaa","nbtjj","nurde","dqldn","uduzc","dcjbi","qfwdf","lefvz","fmmca","xfyzg","hvrmh","ilmlm","remnf","yuijf","qqqnh","tagvi","hdsxn","whnam","ntmyr","tpcyq","yrpny","eptzk","pujbg","celot","jtgef","gvdgp","kboeg","nralc","bpcbq","ezxpc","xdtay","gqmzr","vzuhv","bgrew","dudvt","kuauk","dktbd","qrmpr","nxffe","odfsq","iofrm","tmphf","cwbad","kufdm","ubbtv","ijeft","bftzc","jkoxt","uzlew","swziu","icbum","dbxbp","uraff","fhfzg","rgjok","yfyya","gosak","rhetp","bimce","uvaoh","cxakx","wigyr","jtuif","otvhl","zosxm","wbvfx","vmumu","wsyyl","jjcph","znmmi","vvhkb","prgpa","gzkiw","iktsj","ofiti","ehqxo","qfvns","hqdki","wgbol","lgzmz","moovr","zkdko","bimol","ayujd","mnhqn"}; 
 	 			Random rand = new Random();
-	 			int val = rand.nextInt(3) +1;
+	 			int val = rand.nextInt(1000000) +1;
 	 			int idx = rand.nextInt(4);
 	 			String query = command[idx];
-	 			int idx1 = rand.nextInt(6);
+	 			int idx1 = rand.nextInt(100);
 	 			if(idx==0||idx==1)
-		 			query += bookname[idx1] + Integer.toString(val);	
+		 			query += bookname[idx1] +" " + Integer.toString(val);	
 	 			else if (idx==2)
-                    query += "ka ki 1234567890123 400 1 library123";  
+        {
+          String[] pass = new String[]{"library123","lib"};
+          int id=rand.nextInt(2);
+          int v = rand.nextInt(1);
+          String str ;
+          String isbn ;
+          String auth ;
+          char[] chas = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+          StringBuilder sb2 = new StringBuilder();
+          for (int i = 0; i < 6; i++) {
+              char c = chas[rand.nextInt(chas.length)];
+              sb2.append(c);
+          }
+          auth = sb2.toString();
+          
+          char[] chass = "123456789".toCharArray();
+          StringBuilder sb1 = new StringBuilder();
+          for (int i = 0; i < 13; i++) {
+              char c = chass[rand.nextInt(chass.length)];
+              sb1.append(c);
+          }
+          isbn = sb1.toString();
+          
+          if(v==0)
+            str = bookname[idx1];
+          else 
+          {
+            char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 5; i++) {
+                char c = chars[rand.nextInt(chars.length)];
+                sb.append(c);
+            }
+            str = sb.toString();
+          }
+          int mem = rand.nextInt(4) + 1;
+          int pr = rand.nextInt(1000) + 100; 
+          query += str + " " + auth+ " " + isbn +" " + pr + " " + mem + " " + pass[id];  
+        }
  			 	else
  			 		query += bookname[idx1];
 
@@ -113,8 +151,8 @@ public class client
  
     public static void main(String args[])
     {
-    	  ThreadClass[] clients=new ThreadClass[3];
-     	  for (int i=1;i<=2 ;i++ ) 
+    	  ThreadClass[] clients=new ThreadClass[101];
+     	  for (int i=1;i<=10 ;i++ ) 
      	  {
      	  		clients[i] = new ThreadClass(Integer.toString(i));
       			clients[i].start();	
